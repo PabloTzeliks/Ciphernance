ff8edc3eaba3b01d5c773c729b20be49442bc1da
PabloTzeliks <arq.pabloo@gmail.com>
2026-04-09 16:02:00 -0300

    feat: implement 'AccountStatus' ENUM

--- services/identity-service/src/main/java/io/ciphernance/identity/domain/vo/AccountStatus.java
+++ services/identity-service/src/main/java/io/ciphernance/identity/domain/vo/AccountStatus.java
@@ -1,4 +1,33 @@
 package io.ciphernance.identity.domain.vo;
 
-public class AccountStatus {
+import java.util.Set;
+
+public enum AccountStatus {
+
+    ACTIVE {
+        @Override
+        public Set<AccountStatus> allowedTransitions() {
+            return Set.of(UNDER_ANALYSIS, BLOCKED);
+        }
+    },
+
+    BLOCKED {
+        @Override
+        public Set<AccountStatus> allowedTransitions() {
+            return Set.of();
+        }
+    },
+
+    UNDER_ANALYSIS {
+        @Override
+        public Set<AccountStatus> allowedTransitions() {
+            return Set.of(ACTIVE, BLOCKED);
+        }
+    };
+
+    public abstract Set<AccountStatus> allowedTransitions();
+
+    public boolean canTransitionTo(AccountStatus next) {
+        return allowedTransitions().contains(next);
+    }
 }
