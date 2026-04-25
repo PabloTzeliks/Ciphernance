858b82ca4481be05e3c50c5742f394a63d0efc0b
PabloTzeliks <arq.pabloo@gmail.com>
2026-04-25 10:50:43 -0300

    refactor: #17 reorganize KYC package

--- services/identity-service/src/main/java/io/ciphernance/identity/application/command/user/admin/kyc/KycPromoteUserHandler.java
+++ services/identity-service/src/main/java/io/ciphernance/identity/application/command/user/admin/kyc/KycPromoteUserHandler.java
@@ -1,4 +1,4 @@
-package io.ciphernance.identity.application.command.user.kyc;
+package io.ciphernance.identity.application.command.user.admin.kyc;
 
 import io.ciphernance.identity.application.exception.user.UserNotFoundException;
 import io.ciphernance.identity.application.mediator.CommandHandler;
