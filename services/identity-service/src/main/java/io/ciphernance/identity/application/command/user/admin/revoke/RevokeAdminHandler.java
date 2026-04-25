cad55ce9bd673c668004d4b58c67dffbdb3fc62e
PabloTzeliks <arq.pabloo@gmail.com>
2026-04-25 10:48:21 -0300

    feat: #17 implement Admin revoking logic

---
+++ services/identity-service/src/main/java/io/ciphernance/identity/application/command/user/admin/revoke/RevokeAdminHandler.java
@@ -0,0 +1,44 @@
+package io.ciphernance.identity.application.command.user.admin.revoke;
+
+import io.ciphernance.identity.application.exception.user.UserNotFoundException;
+import io.ciphernance.identity.application.mediator.CommandHandler;
+import io.ciphernance.identity.application.port.out.EventPublisherPort;
+import io.ciphernance.identity.domain.event.DomainEvent;
+import io.ciphernance.identity.domain.event.UserRoleChangedEvent;
+import io.ciphernance.identity.domain.model.User;
+import io.ciphernance.identity.domain.port.UserRepositoryPort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RevokeAdminHandler implements CommandHandler<RevokeAdminCommand, Void> {
+
+    private final UserRepositoryPort userRepository;
+    private final EventPublisherPort eventPublisher;
+
+    public RevokeAdminHandler(UserRepositoryPort userRepository, EventPublisherPort eventPublisher) {
+        this.userRepository = userRepository;
+        this.eventPublisher = eventPublisher;
+    }
+
+    @Override
+    public Void handle(RevokeAdminCommand command) {
+
+        User user = userRepository.findById(command.userId())
+                .orElseThrow(() -> new UserNotFoundException(command.userId()));
+
+        user.revokeAdmin();
+
+        userRepository.save(user);
+
+        eventPublisher.publish(
+                new UserRoleChangedEvent(
+                        DomainEvent.newEventId(),
+                        user.getId(),
+                        user.getRole(),
+                        user.getUpdatedAt()
+                )
+        );
+
+        return null;
+    }
+}
